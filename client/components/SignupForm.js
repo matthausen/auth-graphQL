475cc238acc0f8b381dc5ee1543efcce9ea36fd9
Matteo Fusilli <matteo.fusilli@adarga.ai>
2019-05-08 09:52:31 +0100

    added auth

---
+++ client/components/SignupForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+import mutation from '../mutations/Signup';
+import { graphql } from 'react-apollo';
+
+
+class SignupForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            errors: []
+        }
+    }
+
+
+    onSubmit({ email, password }) {
+        this.props.mutate({
+            variables: { email, password }
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h3>SignUp</h3>
+                <AuthForm
+                    onSubmit={this.onSubmit.bind(this)}
+                />
+            </div>
+        );
+    }
+}
+
+export default graphql(mutation)(SignupForm);
---
+++ client/queries/CurrentUser.js
@@ -0,0 +1,11 @@
+import gql from 'graphql-tag';
+
+export default gqp `
+    {
+        user {
+            id
+            email
+        }
+    }
+
+`;
